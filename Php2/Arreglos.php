d31151b4c5ccc15dc42379255d39b01544252222
EVJimena <jimena13estrada@gmail.com>
2022-03-04 17:08:31 -0600

    cambios

---
+++ Php2/Arreglos.php
@@ -0,0 +1,20 @@
+<?php
+
+$nombre = 'Jimena';
+$nombre2 = 'Michelle';
+//Una variable paracada nombre
+
+//Con un arreglo puede poner varias variables dentro para que sea mas ameno
+//Arreglos indexados:
+$nombres = array('Jimena','Carlos','Pedro');//El array es un contructor
+print_r($nombres);//Imprime lo que viene en el array, mostrando la pocision de estos
+//Imprimir solo un nombre:
+echo $nombres[2];//Imprime el nombre de la pocision 
+
+//Recorrer el arreglo:
+for (si=0 <=2; si++){
+    echo $nombres(si);
+}
+
+
+?>
